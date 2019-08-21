746c26e4a7ac3d1e178149f7fce5cf359f330171
Wes Grimes <wesgrimes@gmail.com>
2019-08-21 16:33:37 -0400

    ci: add linting to husky (#44)

--- lib/in-memory-db.module.ts
+++ lib/in-memory-db.module.ts
@@ -8,7 +8,7 @@ import { InMemoryDBService } from './services';
   exports: [InMemoryDBService],
 })
 export class InMemoryDBModule {
-  static forRoot(config: Partial<InMemoryDBConfig> = {}): DynamicModule {
+  public static forRoot(config: Partial<InMemoryDBConfig> = {}): DynamicModule {
     return {
       module: InMemoryDBModule,
       providers: [
