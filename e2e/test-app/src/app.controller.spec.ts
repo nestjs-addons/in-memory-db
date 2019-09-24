68204acce6f4cf9d3b3fd39c75a2923a552fa88f
jpdesigning <jordan@jpdesigning.com>
2019-09-24 19:20:02 -0400

    chore: rebase master

--- e2e/test-app/src/app.controller.spec.ts
+++ e2e/test-app/src/app.controller.spec.ts
@@ -1,17 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { InMemoryDBService } from '../../../lib/services';
+import { User } from './user';
+import { InMemoryDBModule } from '@nestjs-addons/in-memory-db';
 
 describe('AppController', () => {
   let appController: AppController;
+  let service: InMemoryDBService<User>;
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
-      controllers: [AppController],
-      providers: [AppService],
+      controllers: [AppController, InMemoryDBModule],
+      providers: [],
     }).compile();
 
     appController = app.get<AppController>(AppController);
+    service = app.get(InMemoryDBService);
   });
 
   describe('root', () => {
