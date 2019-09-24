b487f4d140cd0164d2064804c8c09e76360e4a7d
jpdesigning <jordan@jpdesigning.com>
2019-09-24 19:09:29 -0400

    chore: rebase master

--- e2e/test-app/test/app.e2e-spec.ts
+++ e2e/test-app/test/app.e2e-spec.ts
@@ -1,5 +1,4 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { User } from 'src/user';
 import * as request from 'supertest';
 import { AppModule, User } from '../src';
 import { INestApplication } from '@nestjs/common';
